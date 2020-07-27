39c009f034bcaa6844ab672e879f491bb9719a48
fyshhh <41122695+fyshhh@users.noreply.github.com>
2020-07-27 21:26:48 +0800

    Update replay.js

--- replay.js
+++ replay.js
@@ -3,39 +3,6 @@
 /*jslint todo:true*/
 /*global $, jQuery, alert*/
 
-/*
-To Do:
-1) Add card replay support (turn by turn, may require splitting html elements).
-2) UI improvements (look into pictures/animations).
-3) Add advanced options (show/hide all hands, show/hide results).
-
-Changelog:
-140620: Created site and code parsing for play order. (6h)
-150620: Added replay (very basic, shows all tricks and cards played) (6h)
-160620: Added hand viewing and tracking and partner support. (6h)
-170620: Added bid and partner parsing, as well as proper code validation. (6h)
-180620: Added pass support (2h).
-190620: Jesus says you must rest 1 day a week (0h).
-200620: Prototyped player-by-player parsing. (3h)
-210620: Added player-by-player parsing, restructured parsing structure. (7h)
-220620: Began structuring for proper card replay support, added hand sorting, additional code cleanup. (2h)
-230620: Added card replay structuring. (4h)
-240620: Improved upon card replay structuring, added link support (now possible to input a code via link). (4h)
-250520: Added history support. (6h)
-260620: Added tricks notification support and dynamic hand changing. (6h)
-270620: Finished entirety of play support. (8h)
-280620: Finished entirety of bid support. Ready for Milestone 2 deployment. (4h)
-060720: Prototyped new mobile-friendly site (4h).
-100720: Worked on mobile-friendly site (3h).
-110720: Worked on mobile-friendly site (3h).
-220720: add like what a few days inside, say add picture add favicon more bug fixes
-
-hand0 and hand2 are fked need to fix.
-
-use this code: 0CDzG-Y-kmcaNMAEXpqSvyhnoUJLBYzCGswlkdbHIKOuxQRWefjiDFPTrVZgt
-
-*/
-
 "use strict";
 
 var i,
